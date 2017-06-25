4f9b500878f5fbba9840e0f860cb750ef4d6a56f
vvardhan <kvvardhanz@gmail.com>
2017-06-25 17:53:08 -0400

    functional component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React  from  'react';
 import ReactDOM from  'react-dom';
+import SearchBar from './components/search_bar';
 
+
+const API_KEY = 'AIzaSyAsccVJ_MEhkjohdaUdOIhzlAlFUzxj4k8';
 //Create a componenet. this componenet will produce some html.
-const App = function () {
-  return <div> Hi! </div>;
-}
+const App = () =>
+  {
+    return (<div>
+              <SearchBar />
+             </div>
+          );
+  }
 
 //THis will take component's generated HTML  and put it ont he page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
