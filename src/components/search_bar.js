e27f2ee866e8d13f28d9089b32175355947fa99d
vvardhan <kvvardhanz@gmail.com>
2017-06-25 18:23:36 -0400

    search Bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,14 @@
-import React from  'react';
+import React , { Componenet } from  'react';
 
-const SearchBar = () => {
-   return <input /> ;
-};
+class SearchBar extends React.Component {
+     render() {
+       return <input onChange={this.onInputChange} />;
+     }
+
+     onInputChange(event) {
+       console.log(event.target.value);
+     }
+}
+
 
 export default SearchBar;
